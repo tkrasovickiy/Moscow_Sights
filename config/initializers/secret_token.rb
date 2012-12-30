c8cfd03a55198e0b96936c0d7630dc1522657eeb
Tikhon Krasovitskiy <tik714@yandex.ru>
2012-12-30 09:23:16 +0300

    create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MS::Application.config.secret_token = 'df0ef146b6987db227f577a5a0209bf6743045af745d2709d00cd1bbf495a80616295b40481b4693505da6ce29352e421f749ce209740323308bb72bf5cf7c52'
